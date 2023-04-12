4af261efe754f8bde62792c8e9a3056299c2e654
Gabriel Badaró <grbadas@gmail.com>
2023-04-12 00:22:36 -0300

    aula 11 + atividades

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`Velocidade : ${vel}`)
+
+if(vel > 60) {
+    console.log(`TÁ ACHANDO QUE É O RELAMPAGO MARQUINHOS ? MULTADO`)
+}
+console.log(`Tá safe :)`)
---
+++ aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'EUA'
+if (país == 'Brasil') {
+    console.log('Salve meu compatriota :D')
+} else {
+    console.log('Eai, gringo')
+}
